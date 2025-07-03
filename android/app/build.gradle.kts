30637de937e20d6e55ed37dea0b6bc2d65419ade
Rejiane <rejifredrez@gmail.com>
2025-07-03 16:58:56 -0300

    fix ndk version and update mainActivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.ucs_projeto_app_receitas"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- android/app/src/main/kotlin/com/example/ucs_projeto_app_receitas/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ucs_projeto_app_receitas/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.ucs_projeto_app_receitas
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
